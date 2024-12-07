b4b9a4e53eebfaddd564c467960df5269f30a455
wgy040224 <1582316589@qq.com>
2024-12-07 21:48:23 +0800

    搭建OrangeBlog（桔子博客）前端框架

---
+++ orange-blog-vue3/src/main.js
@@ -0,0 +1,22 @@
+import './assets/main.css'
+// 导入 Element Plus 图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'animate.css'
+
+import { createApp } from 'vue'
+import App from '@/App.vue'
+// 导入路由
+import router from '@/router'
+
+const app = createApp(App)
+
+// 应用路由
+app.use(router)
+app.mount('#app')
+
+
+// 引入图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
