8216c1c7d7c48b2210c265ea23c436a726b251f5
wgy040224 <1582316589@qq.com>
2024-12-07 19:14:47 +0800

    搭建OrangeBlog（桔子博客）SpringBoot多模块工程框架

---
+++ orange-blog-springboot/orange-blog-web/src/main/java/com/sqnugy/orangeblog/web/OrangeBlogWebApplication.java
@@ -0,0 +1,13 @@
+package com.sqnugy.orangeblog.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeBlogWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeBlogWebApplication.class, args);
+    }
+
+}
