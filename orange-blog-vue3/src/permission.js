3281379e9221b84d80f7c01a09385e71b84a86ce
wgy040224 <1582316589@qq.com>
2024-12-08 17:30:57 +0800

    拒绝重复登录功能

--- orange-blog-vue3/src/permission.js
+++ orange-blog-vue3/src/permission.js
@@ -9,13 +9,17 @@ router.beforeEach((to, from, next) => {
 
     // 展示页面加载 Loading
     showPageLoading()
-
+    
     // 若用户想访问后台（以 /admin 为前缀的路由）
     // 未登录，则强制跳转登录页
     let token = getToken()
-    if (!token && to.path.startsWith('/admin')) {
-        showMessage('请先登录', 'warning')
-        next({ path: '/login' })
+    if (!token && to.path.startsWith('/admin')) { 
+        // 省略...
+    } else if (token && to.path == '/login') {
+        // 若用户已经登录，且重复访问登录页
+        showMessage('请勿重复登录', 'warning')
+        // 跳转后台首页
+        next({ path: '/admin/index' })
     } else {
         next()
     }
