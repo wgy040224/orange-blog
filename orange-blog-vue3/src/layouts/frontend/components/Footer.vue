c9f83fb2055626b8d46567c71f1fb74228a22a3b
wgy040224 <1582316589@qq.com>
2024-12-13 10:58:10 +0800

    前端：文章详情页、404页及顶部Button组件、文章目录Toc组件封装

--- orange-blog-vue3/src/layouts/frontend/components/Footer.vue
+++ orange-blog-vue3/src/layouts/frontend/components/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="bg-white mt-5 dark:bg-gray-800">
         <div class="w-full mx-auto max-w-screen-xl py-6 px-4 md:flex md:items-center md:justify-between">
             <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://www.quanxiaoha.com/"
-                    class="hover:underline">犬小哈</a>. All Rights Reserved.
+                    class="hover:underline">sqnugy</a>. All Rights Reserved.
             </span>
             <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
